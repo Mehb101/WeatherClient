bddadb24c70f27e85f09bf0703343abb0aa0120b
gl807051 <gulammehboobq@gmail.com>
2024-05-10 21:39:13 -0700

    hey

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { LoginRequest } from './login-request';
-import { Observable, tap } from 'rxjs';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
 import { LoginResult } from './login-result';
 import { environment } from '../../environments/environment';
 
@@ -10,6 +10,23 @@ import { environment } from '../../environments/environment';
 })
 export class AuthService {
 public tokenKey: string= "tokenkey";
+private _authStatus = new BehaviorSubject<boolean>(false);
+
+public authStatus = this._authStatus.asObservable();
+
+  init(): void{
+    if (this.isAuthenticated()){
+        this.setAuthStatus(true);
+    }
+  } 
+  private setAuthStatus(isAuthenticated: boolean): void {
+    this._authStatus.next(isAuthenticated);
+  }
+
+  isAuthenticated(): boolean {
+    return this.getToken() !== null;
+  }
+
   constructor(protected http:HttpClient) {
 
    }
@@ -19,12 +36,16 @@ public tokenKey: string= "tokenkey";
     .pipe(tap(loginResult => {
       if(loginResult.success){
         localStorage.setItem(this.tokenKey, loginResult.token);
+        this.setAuthStatus(true);
       }
     
     }));
   }
         getToken(): string | null {
           return localStorage.getItem(this.tokenKey);
-
+  }
+  logout(): void{
+    localStorage.removeItem(this.tokenKey);
+    this.setAuthStatus(false);
   }
 }
